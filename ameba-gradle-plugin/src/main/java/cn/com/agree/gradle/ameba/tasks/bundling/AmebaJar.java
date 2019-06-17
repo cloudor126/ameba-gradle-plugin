43006219ca662d4c57da9ef5bd36a502d1a0eb35
puyun <pu.yun@agree.com.cn>
2019-06-17 15:45:12 +0800

    清理多余代码

--- ameba-gradle-plugin/src/main/java/cn/com/agree/gradle/ameba/tasks/bundling/AmebaJar.java
+++ ameba-gradle-plugin/src/main/java/cn/com/agree/gradle/ameba/tasks/bundling/AmebaJar.java
@@ -4,15 +4,10 @@
  */
 package cn.com.agree.gradle.ameba.tasks.bundling;
 
-import java.io.File;
 import java.util.Collections;
-import java.util.concurrent.Callable;
 
-import org.gradle.api.Action;
-import org.gradle.api.file.CopySpec;
 import org.gradle.api.file.FileCollection;
 import org.gradle.api.java.archives.Attributes;
-import org.gradle.api.specs.Spec;
 import org.gradle.api.tasks.InputFiles;
 import org.gradle.api.tasks.bundling.Jar;
 
@@ -23,6 +18,20 @@ import org.gradle.api.tasks.bundling.Jar;
 public class AmebaJar extends Jar {
 
 	private FileCollection classpath;
+
+	public void classpath(Object... classpath) {
+		FileCollection existingClasspath = this.classpath;
+		this.classpath = getProject().files((existingClasspath != null) ? existingClasspath : Collections.emptyList(),
+				classpath);
+	}
+
+	/**
+	 * 
+	 */
+	private void configMenifest() {
+		Attributes attributes = getManifest().getAttributes();
+		attributes.putIfAbsent("Hello-World", true);
+	}
 
 	/*
 	 * (non-Javadoc)
@@ -35,36 +44,16 @@ public class AmebaJar extends Jar {
 		super.copy();
 	}
 
-	/**
-	 * 
-	 */
-	private void configMenifest() {
-		Attributes attributes = getManifest().getAttributes();
-		attributes.putIfAbsent("Hello-World", true);
-	}
-
-	public void classpath(Object... classpath) {
-		FileCollection existingClasspath = this.classpath;
-		this.classpath = getProject().files((existingClasspath != null) ? existingClasspath : Collections.emptyList(),
-				classpath);
-	}
-
-	public void setClasspath(Object classpath) {
-		this.classpath = getProject().files(classpath);
+	@InputFiles
+	public FileCollection getClasspath() {
+		return this.classpath;
 	}
 
 	public void setClasspath(FileCollection classpath) {
 		this.classpath = getProject().files(classpath);
 	}
 
-	private Action<CopySpec> classpathFiles(Spec<File> filter) {
-		return (copySpec) -> copySpec
-				.from((Callable<Iterable<File>>) () -> (this.classpath != null) ? this.classpath.filter(filter)
-						: Collections.emptyList());
-	}
-
-	@InputFiles
-	public FileCollection getClasspath() {
-		return this.classpath;
+	public void setClasspath(Object classpath) {
+		this.classpath = getProject().files(classpath);
 	}
 }
